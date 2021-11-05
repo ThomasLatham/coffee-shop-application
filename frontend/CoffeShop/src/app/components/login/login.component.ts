ef9c47b713571bbd1afbdf2c225b2c4b43a2a37b
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-04 21:38:53 -0500

    modified:   frontend/CoffeShop/src/app/components/login/login.component.ts

--- frontend/CoffeShop/src/app/components/login/login.component.ts
+++ frontend/CoffeShop/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/User';
 import { LoginService } from '../../services/login.service';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -29,6 +30,9 @@ export class LoginComponent implements OnInit {
       this.errorMessage();
     }
     
+  }
+  checkUser(){
+    return this.loginServ.currentUser;
   }
 
   clearText(){
