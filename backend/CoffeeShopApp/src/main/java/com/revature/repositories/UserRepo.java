b7f6c563d3f9a8e65d58b960a54d8a34cda7909b
tlatham54 <tlatham54@gmail.com>
2021-11-05 18:29:05 -0400

    Deleting all the test files.

--- backend/CoffeeShopApp/src/main/java/com/revature/repositories/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, Integer> {
-    User findByUsernameAndPassword(String username, String password);
-}
--- backend/CoffeeShopApp/src/main/java/com/revature/repositories/UserRoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.UserRole;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRoleRepo extends CrudRepository<UserRole, Integer> {
-}
