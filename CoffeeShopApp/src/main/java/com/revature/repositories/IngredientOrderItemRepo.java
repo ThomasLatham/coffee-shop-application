4ce2ca9761e19ab7f0b3b0d1f27b6e49faa6e6f2
tlatham54 <tlatham54@gmail.com>
2021-11-07 02:28:25 -0500

    Removing redundant directory.

--- CoffeeShopApp/src/main/java/com/revature/repositories/IngredientOrderItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.IngredientOrderItem;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IngredientOrderItemRepo extends CrudRepository<IngredientOrderItem, Integer> {
-}
--- CoffeeShopApp/src/main/java/com/revature/repositories/MenuItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.MenuItem;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuItemRepo extends CrudRepository<MenuItem, Integer> {
-}
