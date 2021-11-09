6a2b197aeb0b344cc560e3bf0fcef3624912b065
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-09 16:40:59 -0600

    add some files about features testing

---
+++ backend/CoffeeShopApp/src/test/java/com/revature/controllers/UserControllerTests.java
@@ -0,0 +1,36 @@
+//package com.revature.controllers;
+//
+//import com.revature.models.User;
+//import com.revature.services.UserAddressService;
+//import com.revature.services.UserService;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import java.util.List;
+//
+//@AutoConfigureMockMvc
+//@SpringBootTest(classes = com.revature.app.CoffeeShopAppApplication.class)
+//public class UserControllerTests {
+//    @MockBean
+//    UserService us;
+//
+//    @MockBean
+//    UserAddressService uas;
+//
+//    @Autowired
+//    MockMvc mvc;
+//
+//    User u1 = new User("")
+//
+//    @Test
+//    void getAllUsernames() {
+//        Mockito.when(us.getAllUsers()).thenReturn(new List<User>() )
+//    }
+//
+//
+//}
