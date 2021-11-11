5a36fb45cc8527bf059c270de9edebda928dda75
Tutan <tutannegash.abegaz@revature.net>
2021-11-11 15:21:50 -0500

    new edited tests

--- backend/CoffeeShopApp/src/test/java/com/revature/services/ItemCategoryServiceTests.java
+++ backend/CoffeeShopApp/src/test/java/com/revature/services/ItemCategoryServiceTests.java
@@ -4,14 +4,11 @@ import com.revature.models.*;
 import com.revature.repositories.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.text.ParseException;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest(classes = com.revature.app.CoffeeShopAppApplication.class)
 public class ItemCategoryServiceTests {
